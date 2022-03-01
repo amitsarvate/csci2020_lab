352626d673a0d7cdd515b9e23c53669abd440f67
Amit Sarvate <amit@sarvate.com>
2022-03-01 10:03:24 -0500

    finished lab04 working UI with feedback to console

--- lab04/src/main/java/com/example/javafxproject/HelloApplication.java
+++ lab04/src/main/java/com/example/javafxproject/HelloApplication.java
@@ -8,13 +8,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+        stage.setTitle("Lab 04 Form");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- lab04/src/main/java/com/example/javafxproject/HelloController.java
+++ lab04/src/main/java/com/example/javafxproject/HelloController.java
@@ -1,14 +1,51 @@
 package com.example.javafxproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    final String phonePattern  = "(\\d{3}(\\d{3}(\\d{4})";
+    final String phoneFormat = "$1-$2-$3";
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label usernameLabel;
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Label fullNameLabel;
+    @FXML
+    private TextField fullName;
+
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private TextField email;
+
+    @FXML
+    private Label phoneNumberLabel;
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private Label dobLabel;
+    @FXML
+    private DatePicker dob;
+
+    @FXML
+    private Button register;
+
+    public void handleButtonClick() {
+        System.out.println("Full Name: " + fullName.getText());
+        System.out.println("Email: " + email.getText());
+        System.out.println("Phone #: " + phoneNumber.getText());
+        System.out.println("DOB: " + dob.getValue());
     }
+
 }
