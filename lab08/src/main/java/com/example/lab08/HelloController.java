0c1ce9feb6a000a79f19b62dddd3cc54c211eb69
Amit Sarvate <amit@sarvate.com>
2022-03-25 17:10:02 -0400

    commiting lab 08 start and lab05_actual

---
+++ lab08/src/main/java/com/example/lab08/HelloController.java
@@ -0,0 +1,42 @@
+package com.example.lab08;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private Button addbtn;
+
+    @FXML
+    private TextField assigninput;
+
+    @FXML
+    private Label assignlabel;
+
+    @FXML
+    private TextField exminput;
+
+    @FXML
+    private Label exmlabel;
+
+    @FXML
+    private TextField midinput;
+
+    @FXML
+    private Label midlabel;
+
+    @FXML
+    private TextField stuidinput;
+
+    @FXML
+    private Label stuidlabel;
+
+    @FXML
+    private TableView<?> table;
+
+}
+
